68a8d88888f2085e6f23cc8c5c7d98e93fc543c4
Nguyen Tran <me@mac.local>
2025-10-07 18:58:43 -0500

    remove files from gitignore

---
+++ commit.py
@@ -0,0 +1,9 @@
+import os
+import sys
+import shutil
+if sys.argv[1] != "false":
+    os.system("git add .")
+    os.system(f"git commit -m \"{sys.argv[1]}\"")
+    os.system("git push")
+shutil.copy(os.getcwd() + "/engine.py", os.getcwd() + "/chess-engine")
+shutil.copy(os.getcwd() + "/engine.py", os.getcwd() + "/chess-engine-alternate")
---
+++ reset.py
@@ -0,0 +1,3 @@
+import os
+os.system("sudo rm -rf lila-docker")
+os.system("git clone https://github.com/lichess-org/lila-docker.git")
